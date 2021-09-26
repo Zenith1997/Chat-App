bfec247e205950005564eda21ee61824a7b3299d
Dabarera <75031785+Zenith1997@users.noreply.github.com>
2021-09-27 02:16:33 +0530

    changes to home

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (  
+        <div className="home">
+            <div className="body">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores tempore odit sunt qui, distinctio in voluptate commodi, voluptatibus placeat quis cupiditate sed pariatur deleniti obcaecati mollitia, facere quibusdam eos est.</p>
+            </div>
+        </div>
+    );
+}
+ 
+export default Home;
